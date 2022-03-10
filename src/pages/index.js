8f6b9ca4c4d8982b41ec5fa453a72ac312c22a71
Marcel1853 <74411845+Marcel1853@users.noreply.github.com>
2022-03-10 13:23:26 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,6 @@ function Home() {
     <Layout
       title={`${siteConfig.title}`}
       description="Documentation von TheCrazyBot">
-        <html lang="de">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
