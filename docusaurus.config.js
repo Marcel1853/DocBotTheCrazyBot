35b78b399bd2791721248e0aba1cecbbd07f7424
Marcel1853 <74411845+Marcel1853@users.noreply.github.com>
2024-10-07 14:33:25 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'TheCrazyBot Docs',
   tagline: '',
-  url: '/DocBotTheCrazyBot',
+  url: 'https://marcel1853.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
